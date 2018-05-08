f6d6299c7869374e3aa9d3f0e7d00fdd13cf8b72
NA Vu <anhvu01011994@gmail.com>
2018-05-08 14:06:40 +0700

    make it better

--- RbiCloud/settings.py
+++ RbiCloud/settings.py
@@ -127,13 +127,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = (os.path.join(os.path.dirname(__file__), '..', 'static'))
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
